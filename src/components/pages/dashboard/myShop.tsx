0be5f0e31492c8800d48d56086e9b4c28dfa36b4
youngi <youngiyang@163.com>
2019-02-14 16:29:21 +0800

    我的店铺

--- src/components/pages/dashboard/myShop.tsx
+++ src/components/pages/dashboard/myShop.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import LayoutWithMobx from '../../withMobx/LayoutWithMobx';
+import { Paper, TextField, Card, Typography, Divider } from '@material-ui/core';
+import FormCardVertical from '../../stateless/FormCardVertical';
+import ExpansionPanelEditor from '../../stateless/ExpansionPanelEditor';
 
 
 
 const MyShop  = () => {
     return (
         <LayoutWithMobx>
-            <h1>我的店铺</h1>
+            <FormCardVertical style={{
+                width: '60%',
+                paddingBottom: 100,
+                maxWidth: 650,
+                minWidth: 330,
+
+            }}>
+                <div>
+                    <h1>设置我的店铺</h1>
+                </div>
+                    <br/>
+                
+                    <Typography color="textSecondary" gutterBottom>
+                        应用信息
+                    </Typography>
+                    <ExpansionPanelEditor  title="店铺名称"/>
+                    <ExpansionPanelEditor　title="应用LOGO"  />
+
+                    <Divider />                  
+
+               
+
+            </FormCardVertical>
+
             
         </LayoutWithMobx>
     )
