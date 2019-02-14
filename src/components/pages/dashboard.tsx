6a2a2716a6f10f6ebc0455197bed01e6a2178451
simontaosim <xsqfeather@gmail.com>
2019-02-15 04:42:14 +0800

    删除了不必要的代码

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router';
 import { IPageProps } from '../../interfaces/components';
-import BackendLayout from '../withMobx/BackendLayout';
 import Typography from '@material-ui/core/Typography';
-import Layout from '../stateless/Layout';
 import LayoutWithMobx from '../withMobx/LayoutWithMobx';
 
 class Dashboard extends Component<IPageProps> {
@@ -13,15 +11,12 @@ class Dashboard extends Component<IPageProps> {
     handleShowClick = () => this.setState({ visible: true })
     handleSidebarHide = () => this.setState({ visible: false })
     render(){
-        const { visible } = this.state
         return (
             <LayoutWithMobx>
-                <BackendLayout title="控制面板">
                     <Typography component="h2" variant="h4" gutterBottom>
                     控制面板
                     </Typography>
                     
-                </BackendLayout>
                     
             </LayoutWithMobx>
 
