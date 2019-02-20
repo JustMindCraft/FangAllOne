bf0d5e4fa46aa3846ed1e56fb30077f2aaa29661
shindouhiro <shindouhiro@gmail.com>
2019-02-20 22:08:25 +0800

    提现列表页

--- src/components/withMobx/WithdrawalAdminWithMobx.tsx
+++ src/components/withMobx/WithdrawalAdminWithMobx.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import WithdrawalAdminList from '../stateless/WithdrawalAdminList';
 
 class WithdrawalAdminWithMobx extends React.Component{
     render(){
         return(
             <div>
             WithdrawalAdminWithMobx
+            13
+            <WithdrawalAdminList />
             </div>
         )
     }
