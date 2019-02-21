be8f9310f925758d9dc63a3f41d96e0fa563233e
shindouhiro <shindouhiro@gmail.com>
2019-02-21 19:00:08 +0800

    添加提现切换面板

--- src/components/withMobx/WithdrawalAdminWithMobx.tsx
+++ src/components/withMobx/WithdrawalAdminWithMobx.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import WithdrawalAdminList from '../stateless/WithdrawalAdminList';
 import WithdrawalAdminSearch from '../stateless/WithdrawalAdminSearch'
-class WithdrawalAdminWithMobx extends React.Component{
+import WithdrawalAdminTab from '../stateless/WithdrawalAdminTab'
+
+interface IWithdrawalAdminWithMobx{
+    value: number
+}
+class WithdrawalAdminWithMobx extends React.Component<IWithdrawalAdminWithMobx>{
+    state = {
+     value: 1,
+    }
     handleDateChange = (date:any) => {
+
+    }
+    handleChange = () => {
 
     }
     render(){
         return(
             <div>
+                <WithdrawalAdminTab value={this.state.value} handleChange={this.handleChange}/>
                 <WithdrawalAdminSearch handleDateChange={this.handleDateChange} />
                 <WithdrawalAdminList />
             </div>
@@ -15,4 +27,4 @@ class WithdrawalAdminWithMobx extends React.Component{
     }
 }
 
-export default  WithdrawalAdminWithMobx;
+export default WithdrawalAdminWithMobx as any;
