d48b756eb8a20010d357d349f9d8bd9345dcabc7
zuoting <zuoting@10000cars.com>
2019-02-26 18:43:58 +0800

    添加分页

--- src/components/stateless/OrderList.tsx
+++ src/components/stateless/OrderList.tsx
@@ -5,33 +5,32 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
-import TableFooter from '@material-ui/core/TableFooter';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
 import Checkbox from '@material-ui/core/Checkbox';
+import dataContainer from '../../mobx/DataContainer';
 
 const styles = createStyles({
     root: {
-        display: 'flex',
         width: '100%',
         overflow: 'auto',
         margin: '20px'
     },
     table: {
-        fontSize: '36px',
+
     },
     head: {
-        fontStyle: 'bold'
+
     },
     button: {
-
+        margin: '2px'
     }
 })
 
 let id = 0;
-function createdData(orderNo:any, productID:any, orderTime:string, amount:any, payment:any, payTime:any, status:any) {
+function createdData(orderNo: any, productID: any, orderTime: string, amount: any, payment: any, payTime: any, status: any) {
     id += 1;
-    return {id, orderNo, productID, orderTime, amount, payment, payTime, status}
+    return { id, orderNo, productID, orderTime, amount, payment, payTime, status }
 }
 
 const rows = [
@@ -45,64 +44,75 @@ const rows = [
 ]
 
 interface IOrderListProps {
-    classes: any
+    classes: any;
+    handleChangePage: (event: any, page: number) => void;
+    handleChangeRowsPerPage: (event: any) => void;
+    rowsPerPage: number;
+    page: number;
 }
 
-const OrderList = (props:IOrderListProps) => {
+const OrderList = (props: IOrderListProps) => {
+    const { classes } = props;
     return (
+
         <Paper className={props.classes.root}>
             <Table className={props.classes.table}>
-                    <TableHead className={props.classes.head}>
-                        <TableRow>
+                <TableHead className={props.classes.head}>
+                    <TableRow>
+                        <TableCell>
+                            <Checkbox />
+                        </TableCell>
+                        <TableCell>订单号</TableCell>
+                        <TableCell>商品ID</TableCell>
+                        <TableCell>下单时间</TableCell>
+                        <TableCell>金额</TableCell>
+                        <TableCell>支付方式</TableCell>
+                        <TableCell>支付时间</TableCell>
+                        <TableCell>订单状态</TableCell>
+                        <TableCell>操作</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map(row => (
+                        <TableRow key={row.id}>
                             <TableCell>
-                                <Checkbox/>
+                                <Checkbox />
                             </TableCell>
-                            <TableCell>订单号</TableCell>
-                            <TableCell>商品ID</TableCell>
-                            <TableCell>下单时间</TableCell>
-                            <TableCell>金额</TableCell>
-                            <TableCell>支付方式</TableCell>
-                            <TableCell>支付时间</TableCell>
-                            <TableCell>订单状态</TableCell>
-                            <TableCell>操作</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.id}>
-                                <TableCell>
-                                    <Checkbox/>
-                                </TableCell>
-                                <TableCell align='right'>{ row.orderNo }</TableCell>
-                                <TableCell align='right'>{ row.productID }</TableCell>
-                                <TableCell align='right'>{ row.orderTime }</TableCell>
-                                <TableCell align='right'>{ row.amount }</TableCell>
-                                <TableCell align='right'>{ row.payment }</TableCell>
-                                <TableCell align='right'>{ row.payTime }</TableCell>
-                                <TableCell align='right'>{ row.status }</TableCell>
-                                <TableCell>
-                                    <Button color="primary" className={props.classes.button}>
-                                        详情
+                            <TableCell align='right'>{row.orderNo}</TableCell>
+                            <TableCell align='right'>{row.productID}</TableCell>
+                            <TableCell align='right'>{row.orderTime}</TableCell>
+                            <TableCell align='right'>{row.amount}</TableCell>
+                            <TableCell align='right'>{row.payment}</TableCell>
+                            <TableCell align='right'>{row.payTime}</TableCell>
+                            <TableCell align='right'>{row.status}</TableCell>
+                            <TableCell>
+                                <Button color="primary" className={props.classes.button}>
+                                    详情
                                     </Button>
-                                    <Button color="primary">
-                                        发货
+                                <Button color="primary">
+                                    发货
                                     </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                    {/* <TableFooter>
-                        <TableRow>
-                            <TablePagination
-                                rowPerPageOption={[5, 10, 25]}
-                                colSpan={3}
-                            />
+                            </TableCell>
                         </TableRow>
-                    </TableFooter> */}
-                </Table>
+                    ))}
+                </TableBody>
+            </Table>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={props.rowsPerPage}
+                page={props.page}
+                backIconButtonProps={{
+                    'aria-label': 'Previous Page',
+                }}
+                nextIconButtonProps={{
+                    'aria-label': 'Next Page',
+                }}
+                onChangePage={props.handleChangePage}
+                onChangeRowsPerPage={(e:any)=>props.handleChangeRowsPerPage(e)}
+            />
         </Paper>
-        // 尝试使用dx-react-grid-material-ui
-        // 可以完成分页排序过滤分组编辑
     )
 }
 
--- src/components/withMobx/OrderListWithMobx.tsx
+++ src/components/withMobx/OrderListWithMobx.tsx
@@ -3,23 +3,47 @@ import OrderListSearch from '../stateless/OrderListSearch';
 import OrderList from '../stateless/OrderList';
 
 interface IOrdersListWithMobx {
-    store: any
+    classes: any,
 }
 
 class OrderListWithMobx extends React.Component<IOrdersListWithMobx> {
+    state = {
+        rowsPerPage: 5,
+        page: 0,
+    }
+
     handleDateChange = (date: any) => {
 
     }
+
     handleTimeChange = (time: any) => {
 
     }
+
+    handleChangePage = (event: any, page: number) => {
+        console.log(event)
+        console.log(page)
+        this.setState({ page })
+    }
+
+    handleChangeRowsPerPage = (event:any) => {
+        console.log(event.target.value)
+        this.setState({ rowsPerPage:event.target.value})
+    }
+
     render(){
+        const { classes } = this.props;
         return (
             <div>
                 <OrderListSearch 
                     handleDateChange={this.handleDateChange} 
                     handleTimeChange={this.handleTimeChange}/>
-                <OrderList/>
+                <OrderList
+                    handleChangePage={this.handleChangePage}
+                    handleChangeRowsPerPage={(e:any)=>this.handleChangeRowsPerPage(e)}
+                    page={this.state.page}
+                    rowsPerPage={this.state.rowsPerPage}
+                />
             </div>
         )
     }
