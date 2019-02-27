80fdffa4bc5a411870c318955a855a3f4901dd49
shindouhiro <shindouhiro@gmail.com>
2019-02-27 10:40:30 +0800

    添加应用列表

--- src/components/pages/dashboard/application.tsx
+++ src/components/pages/dashboard/application.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LayoutWithMobx from '../../withMobx/LayoutWithMobx';
+import ApplicationAdminWithMobx from '../../withMobx/ApplicationAdminWithMobx';
 
 
-export default class WithdrawalAdminPage extends React.Component {
+export default class ApplicationAdminPage extends React.Component {
     render() {
         return(
             <LayoutWithMobx>
+              <ApplicationAdminWithMobx />
             </LayoutWithMobx>
         )
     }
--- src/components/withMobx/ApplicationAdminWithMobx.tsx
+++ src/components/withMobx/ApplicationAdminWithMobx.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-
-
+import ApplicationAdminList from '../stateless/ApplicationAdminList';
 import { styles } from '../../css/common'
-interface IApplicationAdminWithMobx{
+interface IApplicationAdminWithMobx {
     classes: any,
 }
 
 class ApplicationAdminWithMobx extends React.Component<IApplicationAdminWithMobx>{
+    state = {
+        page: 0,
+        rowsPerPage: 5,
+    }
+    handleChangePage = (event: any, page: number) => {
+        console.log(event)
+        console.log(page)
+        this.setState({ page });
+    }
+
+    handleChangeRowsPerPage = (event: any) => {
+        console.log(event.target.value)
+        this.setState({ rowsPerPage: event.target.value });
+    }
+
     render() {
         const { classes } = this.props;
-        return(
+        return (
             <div className={classes.root}>
-               <h1>我的应用</h1>
+                <ApplicationAdminList handleChangeRowsPerPage={(e: any) => this.handleChangeRowsPerPage(e)}
+                    handleChangePage={this.handleChangePage}
+                    page={this.state.page}
+                    rowsPerPage={this.state.rowsPerPage}
+                />
             </div>
         )
     }
 }
 
-export default  withStyles(styles)(ApplicationAdminWithMobx) as any;
+export default withStyles(styles)(ApplicationAdminWithMobx) as any;
